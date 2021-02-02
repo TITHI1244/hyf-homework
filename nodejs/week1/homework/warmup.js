048488d31228e8850ccc67d9c2fe990ad06af670
TITHI1244 <tithi124410@gmail.com>
2021-02-02 14:33:43 +0100

    added homework folder with the first week homework

---
+++ nodejs/week1/homework/warmup.js
@@ -0,0 +1,22 @@
+console.log("inside warmup file");
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    getDiameter() {
+        return 2 * this.radius;
+    }
+    getCircumference() {
+        return 2 * Math.PI * this.radius;
+    }
+    getArea() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+}
+
+const circle = new Circle(10);
+const diameter = circle.getDiameter();
+const circumference = circle.getCircumference();
+const area = circle.getArea();
+console.log(diameter, circumference, area);
