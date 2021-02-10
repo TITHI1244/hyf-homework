f415613fdaccdec4f0fb9cb441d6e7308568e1dd
TITHI1244 <tithi124410@gmail.com>
2021-02-10 13:49:29 +0100

    changed code to pass the tests

--- nodejs/week2/Meal_sharing/src/backend/api/meals-router.js
+++ nodejs/week2/Meal_sharing/src/backend/api/meals-router.js
@@ -6,8 +6,12 @@ const meals = require("./../data/meals.json");
 // Respond with the json for all the meals & different queries
 router.get("/", async (request, response) => {
   console.log(request.query);
-  const limit = (request.query.limit !== undefined) ? parseInt(request.query.limit) : Math.max(...meals.map(meal => meal.id));
-  const maxPrice = parseInt(request.query.maxPrice) > 0 ? parseInt(request.query.maxPrice) : Math.max(...meals.map(meal => meal.price));
+if((Number.isNaN(parseInt(request.query.maxPrice)) && request.query.maxPrice !== undefined) || (Number.isNaN(parseInt(request.query.limit)) && request.query.limit !== undefined) || (Number.isNaN(Date.parse(request.query.createdAfter)) && request.query.createdAfter !== undefined)) {
+  response.status(400);
+  // response.send({ error: "Pleasye try again with valid values..." });
+}
+  const limit = (request.query.limit === undefined) ? Math.max(...meals.map(meal => meal.id)) : parseInt(request.query.limit);
+  const maxPrice = (request.query.maxPrice !== undefined) ? parseInt(request.query.maxPrice) : Math.max(...meals.map(meal => meal.price)) + 1;
   const title = (request.query.title !== '' && request.query.title !== undefined) ? request.query.title.toLowerCase() : '';
   const firstCreatedDate = Math.min(...meals.map(meal => Date.parse(meal.createdAt)));
   const createdAfter = (request.query.createdAfter !== '' && request.query.createdAfter !== undefined) ? Date.parse(request.query.createdAfter) : firstCreatedDate;
@@ -25,7 +29,11 @@ router.get("/", async (request, response) => {
 router.get("/:id", async (request, response) => {
   try {
     console.log(request.params);
-    const mealWithGivenId = meals.filter(meal => meal.id === parseInt(request.params.id));
+    let givenId = parseInt(request.params.id);
+    if(Number.isNaN(givenId)) {
+      response.status(400)
+    }
+    const mealWithGivenId = meals.filter(meal => meal.id === givenId);
     response.send(...mealWithGivenId);
   } catch (error) {
     throw error;
