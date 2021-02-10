bf29cf68076c4f2194fe3f5082f59611e3a6e210
TITHI1244 <tithi124410@gmail.com>
2021-02-10 14:29:37 +0100

    adjusted spacing between code lines

--- nodejs/week2/Meal_sharing/src/backend/api/meals-router.js
+++ nodejs/week2/Meal_sharing/src/backend/api/meals-router.js
@@ -6,24 +6,23 @@ const meals = require("./../data/meals.json");
 // Respond with the json for all the meals & different queries
 router.get("/", async (request, response) => {
   console.log(request.query);
-if((isNaN(parseInt(request.query.maxPrice)) && request.query.maxPrice !== undefined) || (isNaN(parseInt(request.query.limit)) && request.query.limit !== undefined) || (isNaN(Date.parse(request.query.createdAfter)) && request.query.createdAfter !== undefined)) {
-  response.status(400).send();
-} else {
-  const limit = (request.query.limit === undefined) ? Math.max(...meals.map(meal => meal.id)) : parseInt(request.query.limit);
-  const maxPrice = (request.query.maxPrice !== undefined) ? parseInt(request.query.maxPrice) : Math.max(...meals.map(meal => meal.price)) + 1;
-  const title = (request.query.title !== '' && request.query.title !== undefined) ? request.query.title.toLowerCase() : '';
-  const firstCreatedDate = Math.min(...meals.map(meal => Date.parse(meal.createdAt)));
-  const createdAfter = (request.query.createdAfter !== '' && request.query.createdAfter !== undefined) ? Date.parse(request.query.createdAfter) : firstCreatedDate;
-  try {
-    const filteredMeals = meals.filter(meal => 
-      meal.price < maxPrice && meal.title.toLowerCase().includes(title) && Date.parse(meal.createdAt) >= createdAfter
-    ).slice(0, limit);
-    response.send(filteredMeals);
-  } catch (error) {
-    throw error;
+  if((isNaN(parseInt(request.query.maxPrice)) && request.query.maxPrice !== undefined) || (isNaN(parseInt(request.query.limit)) && request.query.limit !== undefined) || (isNaN(Date.parse(request.query.createdAfter)) && request.query.createdAfter !== undefined)) {
+    response.status(400).send();
+  } else {
+    const limit = (request.query.limit === undefined) ? Math.max(...meals.map(meal => meal.id)) : parseInt(request.query.limit);
+    const maxPrice = (request.query.maxPrice !== undefined) ? parseInt(request.query.maxPrice) : Math.max(...meals.map(meal => meal.price)) + 1;
+    const title = (request.query.title !== '' && request.query.title !== undefined) ? request.query.title.toLowerCase() : '';
+    const firstCreatedDate = Math.min(...meals.map(meal => Date.parse(meal.createdAt)));
+    const createdAfter = (request.query.createdAfter !== '' && request.query.createdAfter !== undefined) ? Date.parse(request.query.createdAfter) : firstCreatedDate;
+    try {
+      const filteredMeals = meals.filter(meal => 
+        meal.price < maxPrice && meal.title.toLowerCase().includes(title) && Date.parse(meal.createdAt) >= createdAfter
+      ).slice(0, limit);
+      response.send(filteredMeals);
+    } catch (error) {
+      throw error;
+    }
   }
-}
-  
 });
 
 // Respond with the json for the meal with the corresponding id
